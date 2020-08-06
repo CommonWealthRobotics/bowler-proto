40a9ec789152b3514fd5116e9587416347f507dc
Octogonapus <firey45@gmail.com>
2020-08-06 00:57:40 -0400

    Switch to opening a PR

--- pr_consumers.jl
+++ pr_consumers.jl
@@ -1,7 +1,7 @@
 using CSV
 using DataFrames
 
-const our_url = "https://github.com/CommonWealthRobotics/bowler-proto.git"
+const our_url = "git@github.com:CommonWealthRobotics/bowler-proto.git"
 const required_tag_prefix = "refs/heads/" # TODO: Revert to "refs/tags/v"
 
 # Derive our tag from the ref
@@ -36,15 +36,22 @@ for row in eachrow(consumers)
             run(`git submodule init`)
         end
 
+        branch_name = "bowlerbot/update-protos"
+        run(`git checkout -b $branch_name`)
+
         # Update the submodule
         cd("bowler-proto")
         run(`git fetch`)
         run(`git checkout $our_tag`)
+        run(`git add .`)
         cd("..")
 
+        # Commit and push
         run(`git commit -am "Update bowler-proto to $our_tag"`)
+        run(`git push --force -u origin $branch_name`)
 
-        run(`curl --request POST --url https://api.github.com/repos/$user_name/$repo_name/dispatches --header 'Authorization: token $(ENV["TOKEN"])' --header 'content-type: application/json' --data '{"event_type":"update-bowler-proto"}'`)
+        # Open a PR
+        run(`curl --request POST --url https://api.github.com/repos/$user_name/$repo_name/pulls --header 'Authorization: token $(ENV["TOKEN"])' --data '{"title":"Update bowler-proto", "head":"$branch_name", "base":"master"}'`)
     finally
         cd(pwd_before)
     end
